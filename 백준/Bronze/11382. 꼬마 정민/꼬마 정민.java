db6f57f77dee0a4ec8860a9b6afddf94e12f942c
leej.young <76792460+JUST7LING@users.noreply.github.com>
2023-07-28 21:01:49 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner s= new Scanner(System.in);
+        long A;
+        A = s.nextLong();
+        A += s.nextLong();
+        A += s.nextLong();
+        
+        System.out.println(A);
+    }
+}
