cd60f22a4ef223fd065286517dc556fe36304617
leej.young <76792460+JUST7LING@users.noreply.github.com>
2023-08-03 17:54:36 +0900

    [Bronze V] Title: 아스키 코드, Time: 212 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args)
+    {
+      Scanner s = new Scanner(System.in);
+      String a = s.nextLine();
+      char c = a.charAt(0);
+      
+      System.out.println((int)c);
+      
+    }
+}
