834632b8a63780e797b5768857422f1e1f3a8a7f
leej.young <76792460+JUST7LING@users.noreply.github.com>
2024-08-30 23:33:57 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 88 ms, Memory: 12756 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -5,15 +5,12 @@ public class Main {
 		 Scanner sc = new Scanner(System.in);
 		 char[] in = sc.next().toCharArray();
 		 
-		 String result = "";
 		 for(int i = 0; i<in.length; i++) {
 			 if((int) in[i]>96) {
-				 result += (char) (in[i]-32);
+				 System.out.print((char) (in[i]-32));
 				 continue;
 			 }
-			 result += (char) (in[i]+32);
+			 System.out.print((char) (in[i]+32));
 		 }
-		 
-		 System.out.println(result);
 	}
 }
