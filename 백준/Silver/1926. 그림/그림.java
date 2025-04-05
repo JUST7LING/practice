d4bfbd344d8b3fc020ec913f7e0da82e549b7e5c
leej.young <76792460+JUST7LING@users.noreply.github.com>
2025-04-05 21:21:32 +0900

    [Silver I] Title: 그림, Time: 380 ms, Memory: 45564 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -11,8 +12,15 @@ public class Main {
 
 	static int[][] canvas;
 	static int pictures, maxSize;
-	static Queue<Integer[]> Q = new LinkedList<>();
+	static Queue<Idx> Q = new LinkedList<>();
 	
+	static class Idx{
+		int row, col;
+		Idx(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+	}
 	
 	static void setter() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,21 +37,27 @@ public class Main {
 		br.close();
 	}
 	
+	static void printOut() {
+		for(int[] rows : canvas) {
+			System.out.println(Arrays.toString(rows));
+		}
+	}
+	
 	static int BFS() {
 		// 상, 하, 좌, 우
 		int size = 1;
 		int[] dr = {1, -1, 0, 0};
 		int[] dc = {0, 0, -1, 1};
 		while(!Q.isEmpty()) {
-			Integer[] curr = Q.poll(); // 꺼내고
-			canvas[curr[0]][curr[1]] = 0; // 방문체크 하고
+			Idx curr = Q.poll(); // 꺼내고
+			canvas[curr.row][curr.col] = 0; // 방문체크 하고
 			for(int i = 0; i<4; i++) {
-				if(curr[0]+dr[i]>=0 && curr[0]+dr[i]<canvas.length) { // 행 이동 체크
-					if(curr[1]+dc[i]>=0 && curr[1]+dc[i]<canvas[0].length) { // 열 이동 체크
-						if(canvas[curr[0]+dr[i]][curr[1]+dc[i]] == 1) {
-							canvas[curr[0]+dr[i]][curr[1]+dc[i]] = 0;
+				if(curr.row+dr[i]>=0 && curr.row+dr[i]<canvas.length) { // 행 이동 체크
+					if(curr.col+dc[i]>=0 && curr.col+dc[i]<canvas[0].length) { // 열 이동 체크
+						if(canvas[curr.row+dr[i]][curr.col+dc[i]] == 1) {
+							canvas[curr.row+dr[i]][curr.col+dc[i]] = 0;
 							size++;
-							Q.add(new Integer[] {curr[0]+dr[i], curr[1]+dc[i]});
+							Q.add(new Idx(curr.row+dr[i], curr.col+dc[i]));
 						}
 					}
 				}
@@ -57,7 +71,7 @@ public class Main {
 		for(int i = 0; i<canvas.length; i++) {
 			for(int j = 0; j<canvas[0].length; j++) {
 				if(canvas[i][j] == 1) { // 새로운 그림을 찾았다!
-					Q.add(new Integer[] {i, j});
+					Q.add(new Idx(i, j));
 					pictures++;
 					int size = BFS();
 					if(size>maxSize) maxSize = size;
