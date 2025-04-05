4bb441765c97ea5d7879760573e49fed2cab2ebb
leej.young <76792460+JUST7LING@users.noreply.github.com>
2025-04-05 21:18:54 +0900

    [Silver I] Title: 그림, Time: 448 ms, Memory: 51468 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,36 +1,32 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	static int[][] canvas;
 	static int pictures, maxSize;
-	static Queue<Idx> Q = new LinkedList<>();
+	static Queue<Integer[]> Q = new LinkedList<>();
 	
-	static class Idx{
-		int row, col;
-		Idx(int row, int col) {
-			this.row = row;
-			this.col = col;
-		}
-	}
 	
-	static void setter() {
-		Scanner sc = new Scanner(System.in);
-		int row = sc.nextInt();
-		int col = sc.nextInt();
+	static void setter() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int row = Integer.parseInt(st.nextToken());
+		int col = Integer.parseInt(st.nextToken());
 		canvas = new int[row][col];
-		for(int i = 0; i<row*col; i++) {
-			canvas[i/col][i%col] = sc.nextInt();
+		for(int i = 0; i<row; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j<col; j++) {
+				canvas[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
-	}
-	
-	static void printOut() {
-		for(int[] rows : canvas) {
-			System.out.println(Arrays.toString(rows));
-		}
+		br.close();
 	}
 	
 	static int BFS() {
@@ -39,15 +35,15 @@ public class Main {
 		int[] dr = {1, -1, 0, 0};
 		int[] dc = {0, 0, -1, 1};
 		while(!Q.isEmpty()) {
-			Idx curr = Q.poll(); // 꺼내고
-			canvas[curr.row][curr.col] = 0; // 방문체크 하고
+			Integer[] curr = Q.poll(); // 꺼내고
+			canvas[curr[0]][curr[1]] = 0; // 방문체크 하고
 			for(int i = 0; i<4; i++) {
-				if(curr.row+dr[i]>=0 && curr.row+dr[i]<canvas.length) { // 행 이동 체크
-					if(curr.col+dc[i]>=0 && curr.col+dc[i]<canvas[0].length) { // 열 이동 체크
-						if(canvas[curr.row+dr[i]][curr.col+dc[i]] == 1) {
-							canvas[curr.row+dr[i]][curr.col+dc[i]] = 0;
+				if(curr[0]+dr[i]>=0 && curr[0]+dr[i]<canvas.length) { // 행 이동 체크
+					if(curr[1]+dc[i]>=0 && curr[1]+dc[i]<canvas[0].length) { // 열 이동 체크
+						if(canvas[curr[0]+dr[i]][curr[1]+dc[i]] == 1) {
+							canvas[curr[0]+dr[i]][curr[1]+dc[i]] = 0;
 							size++;
-							Q.add(new Idx(curr.row+dr[i], curr.col+dc[i]));
+							Q.add(new Integer[] {curr[0]+dr[i], curr[1]+dc[i]});
 						}
 					}
 				}
@@ -56,19 +52,22 @@ public class Main {
 		return size;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		setter();
 		for(int i = 0; i<canvas.length; i++) {
 			for(int j = 0; j<canvas[0].length; j++) {
 				if(canvas[i][j] == 1) { // 새로운 그림을 찾았다!
-					Q.add(new Idx(i, j));
+					Q.add(new Integer[] {i, j});
 					pictures++;
 					int size = BFS();
 					if(size>maxSize) maxSize = size;
 				}
 			}
 		}
-		
-		System.out.printf("%d\n%d", pictures, maxSize);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(pictures + "\n"); // 그림의 개수 출력
+		bw.write(maxSize + "\n");  // 가장 큰 그림의 크기 출력
+		bw.flush();
+		bw.close();
 	}
 }
