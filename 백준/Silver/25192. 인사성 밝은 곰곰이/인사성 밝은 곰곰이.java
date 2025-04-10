fff2400b31d748bdf76c6beee060ce212ff19be6
leej.young <76792460+JUST7LING@users.noreply.github.com>
2025-04-11 03:35:08 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 260 ms, Memory: 29020 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main {
+	static HashSet<String> hi;
+	static String[] logs;
+	static int cursor, result;
+	
+	static void setter() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		logs = new String[Integer.parseInt(br.readLine())];
+		
+		for(int i = 0; i<logs.length; i++) {
+			logs[i] = br.readLine();
+		}
+	}
+	
+	static void greeting() {
+		hi = new HashSet<>();
+		for(int i = 0; i<logs.length; i++) {
+			if(logs[i].equals("ENTER")) {
+				result += hi.size();
+				hi = new HashSet<>();
+				continue;
+			}
+			hi.add(logs[i]);
+		}
+		result += hi.size();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		setter();
+		greeting();
+		System.out.println(result);
+	}
+}
