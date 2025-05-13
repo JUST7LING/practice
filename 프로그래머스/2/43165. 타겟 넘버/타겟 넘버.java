99cde343ba667c4b60f2c4cca7aa1430ac2b496e
leej.young <76792460+JUST7LING@users.noreply.github.com>
2025-05-13 17:02:49 +0900

    [level 2] Title: 타겟 넘버, Time: 8.07 ms, Memory: 95.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,31 @@
+class Solution {
+    static int answer, target;
+    static int[] numbers;
+    
+    public int solution(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.target = target;
+        recursive(0, 0);
+        return answer;
+    }
+    
+    //recursive 
+    // 종료 조건 -> 배열의 숫자만큼만 연산 -> 포인터가 배열을 벗어나면 return
+    static void recursive(int index, int calc){ // 지금 탐색할 배열의 인덱스 / 지금까지의 계산값
+        if(index == numbers.length) {
+            if(calc == target) answer++;
+            return;
+        }
+        
+        int result = calc;
+        
+        result += numbers[index];
+        recursive(index+1, result);
+        result -= numbers[index];
+        
+        result -= numbers[index];
+        recursive(index+1, result);
+        result += numbers[index];
+    }
+        
+}
