58c6b35fefdf8cdf0defdae0ba53a003d6402196
leej.young <76792460+JUST7LING@users.noreply.github.com>
2025-12-04 17:05:03 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.03 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/README.md
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 9번 / 지폐 접기 - 340199 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340199?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340199) 
 
 ### 성능 요약
 
-메모리: 79.4 MB, 시간: 0.04 ms
+메모리: 84.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 16일 17:42:38
+2025년 12월 04일 17:05:00
 
 ### 문제 설명
 
--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -1,25 +1,21 @@
 class Solution {
-    static int[] wallet;
-    
-    public void fold(int[] bill){
-        if(bill[0] > bill[1]) bill[0] /= 2;
-        else bill[1] /= 2;
-    }
-    
-    public boolean check(int[] bill){
-        if(wallet[0] >= bill[0] && wallet[1] >= bill[1]) return true;
-        if(wallet[0] >= bill[1] && wallet[1] >= bill[0]) return true;
-        return false;
-    }
-    
     public int solution(int[] wallet, int[] bill) {
-        this.wallet = wallet;
         int answer = 0;
-        
-        while(!check(bill)){
-            fold(bill);
+        while(needFold(wallet, bill)){
             answer++;
+            if(bill[0] > bill[1]) bill[0] /= 2;
+            else bill[1] /= 2;
         }
         return answer;
+    }
+    
+    boolean needFold(int[] wallet, int[] bill) { // 접어야 한다 = true, 접지 않아도 된다 = false
+        int walletLongIndex = wallet[0] > wallet[1] ? 0 : 1;
+        int billLongIndex = bill[0] > bill[1] ? 0 : 1;
+        int walletShortIndex = walletLongIndex == 1 ? 0 : 1;
+        int billShortIndex = billLongIndex == 1 ? 0 : 1;
+        
+        if(wallet[walletLongIndex] >= bill[billLongIndex] && wallet[walletShortIndex] >= bill[billShortIndex]) return false;
+        return true;
     }
 }
